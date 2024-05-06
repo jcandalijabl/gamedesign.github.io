9a39712a1e46ce677b6d23a0bf43aacad4bd385d
jcandalijabl <jcandalija.smx@institutcampalans.net>
2024-05-06 12:02:03 +0200

    actualitzacio_gran_menus

---
+++ javascript/caixa.js
@@ -0,0 +1,283 @@
+function CaixaAnimacio1(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 0)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio2()',15);
+}
+function CaixaAnimacio2(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 1)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio3()',15);
+}
+function CaixaAnimacio3(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 2)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio4()',15);
+}
+function CaixaAnimacio4(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 3)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio5()',15);
+}
+function CaixaAnimacio5(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 4)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio6()',15);
+}
+function CaixaAnimacio6(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 5)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio7()',15);
+}
+function CaixaAnimacio7(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 6)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio8()',15);
+}
+function CaixaAnimacio8(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 7)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio9()',15);
+}
+function CaixaAnimacio9(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 8)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio10()',15);
+}
+function CaixaAnimacio10(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 9)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio11()',15);
+}
+function CaixaAnimacio11(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 10)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio12()',15);
+}
+function CaixaAnimacio12(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 11)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio13()',15);
+}
+function CaixaAnimacio13(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 12)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio14()',15);
+}
+function CaixaAnimacio14(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 13)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio15()',15);
+}
+function CaixaAnimacio15(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 14)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio16()',15);
+}
+function CaixaAnimacio16(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 15)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio17()',15);
+}
+function CaixaAnimacio17(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 16)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+    setTimeout('CaixaAnimacio18()',15);
+}
+function CaixaAnimacio18(){
+    var caixes = document.getElementsByClassName("caixa_fons");
+    for (i = 0; i < caixes.length; i++)
+    {
+        if (i == 17)
+        {
+            caixes[i].style.visibility = "visible";
+        }
+        else
+        {
+            caixes[i].style.visibility = "hidden";
+        }
+    };
+
+    var boto = document.getElementById("caixa_boto");
+    var boto1 = document.getElementById("caixa_boto_1");
+    var boto2 = document.getElementById("caixa_boto_2");
+    var boto3 = document.getElementById("caixa_boto_3");
+    boto.style.visibility = "hidden";
+    boto1.style.visibility = "visible";
+    boto2.style.visibility = "visible";
+    boto3.style.visibility = "visible";
+}
+function CaixaMostrarMenuDeMenus(){
+    var caixa = document.getElementById("div_caixa_menu_menus");
+    caixa.style.visibility = "visible";
+    caixa.style.opacity = "100%";
+}
